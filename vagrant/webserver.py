0cc41e7ea6cd58ca3f1822719b328d709adad9b4
Alan Quin NG Martinez <AlanQ.NG@cz.abb.com>
2018-10-02 14:26:54 +0200

    Fix bug webserver.py

--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -13,7 +13,7 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output += "<h1>Hello!</h1>"
                 output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
                 output += "</body></html>"
-                self.wfile.write(output)
+                self.wfile.write(output.encode())
                 print (output)
                 return
 
@@ -26,7 +26,7 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output += "<h1>&#161 Hola !</h1>"
                 output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
                 output += "</body></html>"
-                self.wfile.write(output)
+                self.wfile.write(output.encode())
                 print (output)
                 return
 
@@ -47,9 +47,9 @@ class webserverHandler(BaseHTTPRequestHandler):
             output += "<html><body>"
             output += " <h2> Okay, how about this: </h2>"
             output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''.encode()
             output += "</body></html>"
-            self.wfile.write(output)
+            self.wfile.write(output.encode())
             print (output)
         except:
             pass
